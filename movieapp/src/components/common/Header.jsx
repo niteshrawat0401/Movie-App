d9dd1c6c59807eecb5e905470b6e4d7f864d3930
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-05-23 00:33:44 +0530

    file created

---
+++ movieapp/src/components/common/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Home } from '../../pages/Home'
+import {AppBar, Toolbar} from "@mui/material"
+
+export const Header = () => {
+  return (
+        <></>
+    )
+}
---
+++ movieapp/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Header } from '../components/common/Header'
+
+export const Home = () => {
+  return (
+    <div><Header/></div>
+  )
+}
