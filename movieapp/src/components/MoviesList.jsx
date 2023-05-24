0a9a390a1435f98a5be0c6b035e88367ac67a830
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-05-24 23:55:19 +0530

    category data map

--- movieapp/src/components/MoviesList.jsx
+++ movieapp/src/components/MoviesList.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { List, ListItem, Typography, styled } from '@mui/material'
+import React from 'react';
+
+const Banner = styled('img')({
+    width: 47
+})
+const Container = styled(List)({
+    display: 'flex'
+})
 
 export const MoviesList = ({movie}) => {
   return (
-    <div>
+    <>
         {
             movie.map((ele)=>(
-                
+                <Container>
+                    <ListItem>
+                        <Banner src={`https://image.tmdb.org/t/p/original/${ele.poster_path}`} alt='poster'/>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>{ele.original_title}</Typography>
+                    </ListItem>
+                </Container>
             ))
         }
-    </div>
+    </>
   )
 }
